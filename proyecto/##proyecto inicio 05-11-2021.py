2bc086e8ddabd151db3dffbf8bd55f215ea4c6ab
Davidhfxd <91155443+Davidhfxd@users.noreply.github.com>
2021-11-13 00:59:45 -0300

    datos a grafo
    Los datos ya estan en el grafo XD

--- proyecto/##proyecto inicio 05-11-2021.py
+++ proyecto/##proyecto inicio 05-11-2021.py
@@ -1,47 +1,47 @@
 ##proyecto inicio 05-11-2021
 import requests
-import pandas as pd
-l1=pd.read_excel("esta.xlsx")
-asd1=l1.columns.array
-xx=[]
-for j in asd1:
-    xx.append(j)
-print(xx)
+##import pandas as pd
+##l1=pd.read_excel("esta.xlsx")
+##asd1=l1.columns.array
+##xx=[]
+##for j in asd1:
+##    xx.append(j)
+##print(xx)
 
 
-#k1="AIzaSyD_VKKZ2ZyZIJ9DaBto4IpK2Z8lj2HInJ0"
+k1="AIzaSyD_VKKZ2ZyZIJ9DaBto4IpK2Z8lj2HInJ0"
 
-#url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=Metro%Einstein&destinations=Metro%Zapadores&key=AIzaSyD_VKKZ2ZyZIJ9DaBto4IpK2Z8lj2HInJ0"
-
-
-#response = requests.get(url)
-
-#print(response.text)
-
-import pandas as pd
-
-MAX_value = 999999
-
-l1=pd.read_excel("kk.xlsx")
-
-asd1=l1.columns.array
-
-xx=[]
+url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=Metro%Einstein&destinations=Metro%Zapadores&key=AIzaSyD_VKKZ2ZyZIJ9DaBto4IpK2Z8lj2HInJ0"
 
 
-for j in asd1:
-    xx.append(j)
+response = requests.get(url)
 
-xx.pop(0)
+print(response.text)
 
-grafo=[]
-for k in xx:
-    t1=l1[k]
-    t2=[]
-    for j in t1:
-        t2.append(j)
-    grafo.append(t2)
-print(grafo)
+##import pandas as pd
+##
+##MAX_value = 999999
+##
+##l1=pd.read_excel("kk.xlsx")
+##
+##asd1=l1.columns.array
+##
+##xx=[]
+##
+##
+##for j in asd1:
+##    xx.append(j)
+##
+##xx.pop(0)
+##
+##grafo=[]
+##for k in xx:
+##    t1=l1[k]
+##    t2=[]
+##    for j in t1:
+##        t2.append(j)
+##    grafo.append(t2)
+##print(grafo)
 
 
 
